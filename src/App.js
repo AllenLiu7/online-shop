fd46e323387d10895cfa45935aed51c70183a04c
Allen Liu <allen6qi@gmail.com>
2020-04-02 13:44:57 +0800

    finished homepage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./App.css";
-import HomePage from "./components/homepage.component.jsx";
+import HomePage from "./pages/homepage.component.jsx";
 
 function App() {
   return (
     <div>
-      <HomePage></HomePage>
+      <HomePage />
     </div>
   );
 }
