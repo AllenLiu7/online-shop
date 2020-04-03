082eabaa638760c98c48705c09059e4b3dad3e55
Allen Liu <allen6qi@gmail.com>
2020-04-03 18:39:51 +0800

    built shop-page and supply data to it

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "./App.css";
 import HomePage from "./pages/homepage.component.jsx";
+import shopPage from "./components/shop/shop-page.component";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={shopPage} />
+      </Switch>
     </div>
   );
 }
---
+++ src/components/shop/shop-page.component.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import collections from "./shopitem.data";
+import CollectionPreview from "../collection-preview/collection-preview.component";
+
+class ShopPage extends Component {
+  state = { collections };
+
+  render() {
+    return (
+      <div className="shop-page">
+        {this.state.collections.map(({ id, ...othercollectionprops }) => (
+          //...other equals to title={title} item={items} same key value names
+          <CollectionPreview key={id} {...othercollectionprops} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default ShopPage;
