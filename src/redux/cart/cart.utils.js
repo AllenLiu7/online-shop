b5ace64589f2334152dbea43ecaca0dd769558e6
Allen Liu <allen6qi@gmail.com>
2020-04-13 09:29:55 +0800

    finish setting the cart utils function for adding items to cart

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,1 +1,16 @@
-export const addItemToCart = (cartItems, newItem) => {};
+export const addItemToCart = (cartItems, newItem) => {
+  const existItem = cartItems.find((cartItem) => cartItem.id === newItem.id);
+
+  if (existItem) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === newItem.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+
+  return [...cartItems, { ...newItem, quantity: 1 }];
+};
+
+//if the new item doesn`t exist, assign the quantity to 1
+//otherwise, quatity + 1
