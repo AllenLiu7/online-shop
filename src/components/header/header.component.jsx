c855155902e2c9b0aa9bc732a4892da89b14eed6
Allen Liu <allen6qi@gmail.com>
2020-08-12 10:24:53 +0800

    removed contact in header

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,35 +1,33 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { connect } from "react-redux";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-import { toggleCartHidden } from "../../redux/cart/cart.actions";
-import { signOutStart } from "../../redux/user/user.action";
-import { selectHiddenStatus } from "../../redux/cart/cart.selectors";
-import { selectCurrentUser } from "../../redux/user/user.seletors";
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
+import { signOutStart } from '../../redux/user/user.action';
+import { selectHiddenStatus } from '../../redux/cart/cart.selectors';
+import { selectCurrentUser } from '../../redux/user/user.seletors';
 
-import { ReactComponent as Logo } from "../../assets/crown.svg";
-import CartIcon from "../cart-icon/cart-icon.component";
-import CartDropdown from "../cart-dropdown/cart-dropdown.component";
-import "./header.styles.scss";
+import { ReactComponent as Logo } from '../../assets/crown.svg';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+import './header.styles.scss';
 
 const Header = ({ currentUser, hidden, toggleCartHidden, signOutStart }) => (
-  <div className="header">
-    <Link className="logo-container" to="/">
-      <Logo className="logo" />
+  <div className='header'>
+    <Link className='logo-container' to='/'>
+      <Logo className='logo' />
     </Link>
-    <div className="options">
-      <Link className="option" to="/shop">
+    <div className='options'>
+      <Link className='option' to='/shop'>
         SHOP
       </Link>
-      <Link className="option" to="/shop">
-        CONTACT
-      </Link>
+
       {currentUser ? (
-        <div className="option" onClick={signOutStart}>
+        <div className='option' onClick={signOutStart}>
           SIGN OUT
         </div>
       ) : (
-        <Link to="/sign">SIGN IN</Link>
+        <Link to='/sign'>SIGN IN</Link>
       )}
       <CartIcon onClick={toggleCartHidden} />
       {hidden ? null : <CartDropdown />}
