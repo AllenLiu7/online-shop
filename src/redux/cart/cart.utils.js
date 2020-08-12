d10871d230a3c11a79a358c9e85e0e435f11e27b
Allen Liu <allen6qi@gmail.com>
2020-08-12 10:20:19 +0800

    added comment to cart utils

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,6 +1,7 @@
 export const addItemToCart = (cartItems, newItem) => {
+  //every item has their own id, chech if there is already a same item in the cart
   const existItem = cartItems.find((cartItem) => cartItem.id === newItem.id);
-
+  //if there are same items, find the item and increase its quantity
   if (existItem) {
     return cartItems.map((cartItem) =>
       cartItem.id === newItem.id
@@ -8,15 +9,17 @@ export const addItemToCart = (cartItems, newItem) => {
         : cartItem
     );
   }
-
+  //otherwise add the new item and set the quantity to 1
   return [...cartItems, { ...newItem, quantity: 1 }];
 };
 
 export const removeItem = (cartItems, itemToRemove) => {
+  //create an array that does not have the item which needs to be removed
   return cartItems.filter((cartItem) => cartItem.id !== itemToRemove.id);
 };
 
 export const increaseItem = (cartItems, itemToIncrease) => {
+  //increase the quantity if the id is exist
   return cartItems.map((cartItem) =>
     cartItem.id === itemToIncrease.id
       ? { ...cartItem, quantity: cartItem.quantity + 1 }
@@ -25,6 +28,8 @@ export const increaseItem = (cartItems, itemToIncrease) => {
 };
 
 export const decreaseItem = (cartItems, itemTodecrease) => {
+  //in the checkout page, if the item has more than 1 quantity, find this item in cartItem and decrease the quantity by 1
+  //if the quantity = 1, remove the item.
   if (itemTodecrease.quantity > 1) {
     return cartItems.map((cartItem) =>
       cartItem.id === itemTodecrease.id
